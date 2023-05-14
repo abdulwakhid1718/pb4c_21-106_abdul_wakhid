d1d00261a87b013a0fcd48a70061c0a56ff2451d
abdulwakhid1718 <abdulwakhid103@gmail.com>
2023-05-14 23:19:01 +0700

    hapus Kesalahan

--- modul5_fragment/praktikum/TabLayoutWA/app/src/main/java/com/example/tablayoutwa/fragment/data/Chat.kt
+++
@@ -1,8 +0,0 @@
-package com.example.tablayoutwa.fragment.data
-
-data class Chat(
-    val nama: String,
-    val chat: String,
-    val status: Int,
-    val tgl: String
-)
--- modul5_fragment/praktikum/TabLayoutWA/app/src/main/java/com/example/tablayoutwa/fragment/data/Status.kt
+++
@@ -1,7 +0,0 @@
-package com.example.tablayoutwa.fragment.data
-
-data class Status(
-    val nama: String,
-    val img: Int,
-    val tgl: String
-)
